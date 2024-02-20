8f26a33e692fef1d222f15de0e1c83e419f65ea8
AmulyaDukunta <amulya.d@beehyv.com>
2024-02-20 17:00:39 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'PricelessTreasures', 'static'),
+    os.path.join(BASE_DIR,'static'),
 ]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
