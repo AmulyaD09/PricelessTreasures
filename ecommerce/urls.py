6a5c1df174555d1fd03d5e326b2a3bced4f92b3c
AmulyaDukunta <amulya.d@beehyv.com>
2024-02-20 17:07:18 +0530

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,4 @@
 
-from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
@@ -52,5 +51,3 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
